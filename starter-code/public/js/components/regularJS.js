679484747a92de2a73f3dbcc1c495cc2d420b0d2
Ana Nikolova <ani.nik19@gmail.com>
2019-06-02 05:53:02 +0200

    select a pokemon

--- starter-code/public/js/components/regularJS.js
+++ starter-code/public/js/components/regularJS.js
@@ -1,13 +1,56 @@
-webpackJsonp([0],{
+var pokemonsEl = document
+  .querySelector(".select-screen")
+  .querySelectorAll(".character");
 
-/***/ 99:
-/***/ (function(module, exports, __webpack_require__) {
+console.log(pokemonsEl);
+var i = 0;
+while (i < pokemonsEl.length) {
+  pokemonsEl[i].onclick = function() {
+    var pokemonName = this.dataset.pokemon;
+    console.log("I pressed this polemon " + pokemonName);
+  };
+  i++;
+}
 
-"use strict";
+// webpackJsonp([0],[
+// /* 0 */
+// /***/ (function(module, exports, __webpack_require__) {
 
+// "use strict";
 
-console.log('Welcome To The Rocky Stack');
+// console.log('Welcome To The Rocky Stack');
+// // pokemon
+// // create data for 3 different pokemons, with their names, type, weaknesses, health, and attack moves(name, attack stat, maximum)
+// var pokemons = [{
+//   name: 'charmander',
+//   type: 'fire',
+//   attack: 52,
+//   stamina: 39,
+//   level: 1
+// }, {
+//   name: 'charmander',
+//   type: 'fire',
+//   attack: 52,
+//   stamina: 39,
+//   level: 1
+// }];
 
-/***/ })
+// var attack = 20;
+// var level = 10;
+// var stack = 1.3;
+// var stamina = 39;
 
-},[99]);
+// // create a formula for attacks
+// console.log(attack * level * stack / 7);
+
+// // create a formula for health
+// //HP = 0.20 x Sqrt(Pokemon_level) x (HP_base_stat)
+// console.log(0.20 * Math.sqrt(level) * stamina * 15);
+
+// // let user choose 1 and then assign a random pokemon to battle thats not the users pokemon
+// // p1 vs p2
+
+// // when one user loses all his health declare a winner
+
+// /***/ })
+// ],[0]);
